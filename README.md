9cf0f99b170ea1b24bed2e0fad4f20697aa1f6e1
suhoon1020 <suhoon1020@naver.com>
2025-05-07 03:29:42 +0900

    점핏 디테일 페이지 전체 크롤링 기능능

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ pip3 install tqdm
 pip3 install pandas
 pip3 install beautifulsoup4
 pip3 install lxml
+pip3 install matplotlib
 ```
--- Source_Files/Jumpit_Crawling_Functions.py
+++ Source_Files/Jumpit_Crawling_Functions.py
@@ -5,9 +5,14 @@ from tqdm import tqdm
 import pandas as pd
 import json
 from bs4 import BeautifulSoup
-
+import re
 # 점핏 메인 페이지의 url입니다
 MAIN_URL = "https://jumpit-api.saramin.co.kr/api/positions?sort=rsp_rate&highlight=false&page="
+DETAIL_URL = 'https://jumpit.saramin.co.kr/position/'
+
+TITLE_CLASS = "sc-923c3317-0 jIWuEG"
+MAIN_CLASS = "position_info"
+BLIND_CLASS = "sc-b12ae455-1 hvXrQd"
 
 '''
 점핏 메인 페이지를 가져오는 함수입니다
@@ -113,3 +118,117 @@ def toCsvFile(dataFrame, date):
 
 def toJsonFile(dataFrame, date):
     dataFrame.to_json(f"../Data_Files/Crawling_DataFile_MainPage_json_{date}.txt",force_ascii=False)
+
+'''
+점핏 상세 페이지를 가져오는 함수입니다
+각각의 상세 페이지 정보를 가져옵니다
+'''
+def improveText(text):
+    text = text.replace('\n', ' ')  # 줄바꿈 문자 제거
+    text = text.replace('\r', '')  # 캐리지 리턴 문자 제거
+    text = text.replace('\t', '')  # 탭 문자 제거
+    re.sub(r'[^a-zA-Z0-9가-힣\s]', '', text)  # 특수문자 제거
+
+    return text.strip()  # 양쪽 공백 제거
+
+def getDetailPage(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")  # lxml Parser 사용
+    body = soup.find('body')
+    return body
+
+def getTitleInfo(body):
+    titleInfo = body.find_all('div', class_ = TITLE_CLASS)
+    titles = []
+    company = []
+    pride = []
+    for info in titleInfo:
+        h1_tag = info.find('h1')
+        company_tag = info.find('a')
+        if h1_tag:  # <h1> 태그가 존재하는 경우에만 추가
+            titles.append(h1_tag.text)
+
+        if company_tag:
+            company.append(company_tag.text)
+
+    for info in titleInfo:
+        pride_tags = info.find_all('li')
+        for pride_tag in pride_tags:
+            pride.append(pride_tag.text)
+    
+    return titles, company, pride
+
+
+def getMainInfo(body):
+    mainWork = ''
+    requirements = ''
+    preferential = ''
+    welfare = ''
+    procedures = ''
+    mainInfo = body.find_all('div', class_ = MAIN_CLASS)
+    techStack = mainInfo[0].find_all('div', class_ = "sc-d9de2de1-0 hMgXwE")
+    techStack = [i.text for i in techStack] # 기술 스택
+
+    allPre = mainInfo[0].find_all('pre')  
+    allPre = [i.text for i in allPre]
+
+    for i in range(len(allPre)):
+        if i == 1:
+            mainWork = allPre[i]
+        elif i == 2:
+            requirements = allPre[i]
+        elif i == 3:
+            preferential = allPre[i]
+        elif i == 4:
+            welfare = allPre[i]
+        elif i == 5:
+            procedures = allPre[i]
+
+
+    return techStack, mainWork, requirements, preferential, welfare, procedures
+
+def getBlindInfo(body):
+    blindInfo = body.find_all('dl', class_ = "sc-b12ae455-1 hvXrQd")
+    career = blindInfo[0].find('dd')
+    career = [i.text for i in career]  # 경력
+    education = blindInfo[1].find('dd')
+    education = [i.text for i in education]  # 학력
+    closedAt = blindInfo[2].find('dd')
+    closedAt = [i.text for i in closedAt]  # 마감일
+    jobLocation = blindInfo[3].find('li')
+    jobLocation = [i.text for i in jobLocation]  # 근무지
+
+    return career, education, closedAt, jobLocation
+
+def crawlingDetailPage():
+    positions = getJumpitPositions()
+    ids = getIDs(positions)
+
+    for i in range(0,3):
+        data = []
+        url = DETAIL_URL + str(ids[i])
+        print(f"상세 페이지 {i+1} : {url}")
+        time.sleep(0.3)
+
+        body = getDetailPage(url)
+        title, company, pride = getTitleInfo(body)
+        techStack, mainWork, requirements, preferential, welfare, procedures = getMainInfo(body)
+        career, education, closedAt, jobLocation = getBlindInfo(body)
+
+        data.append({
+            'title': title,
+            'company': company,
+            'pride': pride,
+            'techStack': techStack,
+            'mainWork': mainWork,
+            'requirements': requirements,
+            'preferential': preferential,
+            'welfare': welfare,
+            'procedures': procedures,
+            'career': career,
+            'education': education,
+            'closedAt': closedAt,
+            'jobLocation': jobLocation
+        })
+
+    return data
